d4605e394fd1356406bb0007513d6403e97ae988
Khatech01 <s-ahk@live.fr>
2022-06-28 21:11:54 +0100

    Console.log added

--- session-2/index.js
+++ session-2/index.js
@@ -1,2 +1,5 @@
 // This is a comment in JavaScript
 // You can see where this file is connected to your HTML in the index.html on line 14
+console.log(document.body.parentNode)
+console.log(document.getElementById ('description'))
+console.log(document.getElementsByClassName('.about'))
