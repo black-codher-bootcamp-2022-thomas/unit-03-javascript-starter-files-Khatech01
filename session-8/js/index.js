05f13997297763142c488d7a85ec40404a3f1dd2
Khatech01 <s-ahk@live.fr>
2022-07-12 18:57:02 +0100

    Task 1

--- session-8/js/index.js
+++ session-8/js/index.js
@@ -1,3 +1,12 @@
 import displayPopulation from "./birmingham.js";
 import { fruits } from "./fruit.js";
 import createListOfFruits from "./giveMeFruits.js";
+
+
+// function myFunction() {
+    var button = document.createElement("button");
+    var textNode = document.createTextNode("Show Population");
+    button.appendChild(textNode);
+    button.onclick = displayPopulation;
+    document.body.appendChild(button);
+//   }
